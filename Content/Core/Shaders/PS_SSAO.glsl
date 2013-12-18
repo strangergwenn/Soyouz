f3cfe4d6eb50b476b7e0628d0e8880496bb47140
Gwennaël ARBONA <gwennael.arbona@gmail.com>
2013-12-18 21:08:03 +0100

    [DEV] Further SSAO tuning

--- Content/Core/Shaders/PS_SSAO.glsl
+++ Content/Core/Shaders/PS_SSAO.glsl
@@ -11,24 +11,16 @@
 
 #version 150
 
-// Samplers
 uniform sampler2D sSceneDepthSamplerHigh;
 uniform sampler2D sSceneDepthSamplerLow;
 uniform sampler2D sRotSampler4x4;
 
-// Performance parameters
-uniform int cSampleCount;
-uniform float cRangeCompression;
-
-// Artistic parameters
-uniform float cSampleLengthScreenSpace;
-uniform float cOffsetScale;
-uniform float cDefaultAccessibility;
-uniform float cEdgeHighlight;
-
-// Viewport
-uniform float cFov;
 uniform vec4 cViewportSize;
+uniform float cFov;
+uniform float cStrength;
+uniform float cOffsetScale;
+uniform float cRangeCompression;
+uniform float cSampleLengthScreenSpace;
 
 
 /*-------------------------------------------------
@@ -56,37 +48,32 @@ void main()
 	vec3 rotationVector = 2.0 * texture2D(sRotSampler4x4, rotationTC).xyz - 1.0;
     
 	// Initialize data
-	float r = tan(cSampleLengthScreenSpace * cFov) * fragmentWorldDepth;
-	float sampleLength = cOffsetScale;
-	float sampleLengthStep = pow((cSampleLengthScreenSpace / sampleLength), 1.0f/cSampleCount);
 	float accessibility = 0;
+	float sampleLength = cOffsetScale;
+	float r = tan(cSampleLengthScreenSpace * cFov) * fragmentWorldDepth;
+	float sampleLengthStep = pow((cSampleLengthScreenSpace / sampleLength), 0.1f);
 
 	// Loop on samples
-	for (int i = 0; i < (cSampleCount / 8); i++)
+	for (int x = -1; x <= 1; x += 2)
+	for (int y = -1; y <= 1; y += 2)
+	for (int z = -1; z <= 1; z += 2)
 	{
-		for (int x = -1; x <= 1; x += 2)
-		for (int y = -1; y <= 1; y += 2)
-		for (int z = -1; z <= 1; z += 2)
-		{
-			// Compute coordinates for depth lookup
-			vec3 offset = normalize(vec3(x, y, z)) * sampleLength;
-			sampleLength *= sampleLengthStep;
-			vec3 rotatedOffset = reflect(offset, rotationVector);
-			vec2 sampleTC = vUv0 + rotatedOffset.xy * cSampleLengthScreenSpace;
+		// Compute coordinates for depth lookup
+		vec3 offset = normalize(vec3(x, y, z)) * sampleLength;
+		sampleLength *= sampleLengthStep;
+		vec3 rotatedOffset = reflect(offset, rotationVector);
+		vec2 sampleTC = vUv0 + rotatedOffset.xy * cSampleLengthScreenSpace;
             
-			// Get depth from bi-buffer
-			float hSDepth = 256 * texture2D(sSceneDepthSamplerHigh, sampleTC).w;
-			float lSDepth = texture2D(sSceneDepthSamplerLow, sampleTC).w;
-			float sampleWorldDepth = (hSDepth + lSDepth) * cRangeCompression;
+		// Get depth from bi-buffer
+		float hSDepth = 256 * texture2D(sSceneDepthSamplerHigh, sampleTC).w;
+		float lSDepth = texture2D(sSceneDepthSamplerLow, sampleTC).w;
+		float sampleWorldDepth = (hSDepth + lSDepth) * cRangeCompression;
             
-			// Compute accessibility
-			float fRangeIsInvalid = clamp((fragmentWorldDepth - sampleWorldDepth) / r, 0.0, 1.0);
-			accessibility += mix(float(sampleWorldDepth > (fragmentWorldDepth + rotatedOffset.z * r)) , cDefaultAccessibility, fRangeIsInvalid);
-		}
+		// Compute accessibility
+		float fRangeIsInvalid = clamp((fragmentWorldDepth - sampleWorldDepth) / r, 0.0, 1.0);
+		accessibility += mix(float(sampleWorldDepth > (fragmentWorldDepth + rotatedOffset.z * r)) , 0.0, fRangeIsInvalid);
 	}
-    
-	// Final setup
-	accessibility /= cSampleCount;
-	accessibility *= cEdgeHighlight;
+
+	accessibility *= cStrength;
 	pPixel = vec4(accessibility, accessibility, accessibility, 1.0);
 }
